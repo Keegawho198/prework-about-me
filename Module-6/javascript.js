204a4f0162a8331fff128a679c51540b1d4eec39
Keegan Nair <keegan.nair@hotmail.com>
2019-07-29 17:02:19 +1000

    submiting completed modules

---
+++ Module-6/javascript.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+
+        document.getElementById("growBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+            
+        });
+
+        document.getElementById("resetBtn").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+
+        });
+                 /* why does this not work */
+    </script>
